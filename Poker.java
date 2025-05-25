dbe960c1e0688fb9d5ac09279eef4032ad592a33
dexterchn <dexterchn@outlook.com>
2025-05-25 15:01:59 -0700

    added a drawcard function

--- Poker.java
+++ Poker.java
@@ -1,16 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
 
-
-public class Poker {
+public class Poker{
     //Initialize variables
     private int suit[] = {1, 2, 3, 4};
-    private int cardHolder[][] = {{-1, -1 , -1, -1}, {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, 
-                                {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, 
-                                {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, 
-                                {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, {-1, -1 , -1, -1}, {-1, -1 , -1, -1}};
-    for(int i=0; i<4; i++){
-        for(int j=0; j<13; j++){
-            cardHolder[i][j]=-1;
+    private int cardHolder[][];
+    private int players;
+    private int playerCards[][];
+    private ArrayList<Integer> sharedCards;
+
+    //initialize game
+    public  Poker (int playercnt){
+        for(int i=0; i<4; i++){
+            for(int j=0; j<13; j++){
+                cardHolder[i][j]=-1;
+            }
         }
+        players=playercnt;
+        playerCards= new int[players][2];
+        for(int i=0; i<playercnt; i++){
+            playerCards[i][0]=drawCard(i);
+            playerCards[i][1]=drawCard(i);
+        }
+        sharedCards.add(drawCard(0));
+        sharedCards.add(drawCard(0));
+        sharedCards.add(drawCard(0));
+    }
+
+    //draws a card for a given player
+    public int drawCard(int player){
+        int suitrand=(int)(Math.random()*4);
+        int numrand=(int)(Math.random()*13);
+        while(cardHolder[suitrand][numrand]!=-1){
+            suitrand=(int)(Math.random()*4);
+            numrand=(int)(Math.random()*13);
+        }
+        cardHolder[suitrand][numrand]=player;
+        return suitrand*13+numrand;
+    }
+
+    public int (){
+
     }
 }
+
